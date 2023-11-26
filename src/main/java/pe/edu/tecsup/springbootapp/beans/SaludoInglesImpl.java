a9603d8b7120a8f74827a2e5994b46dc9b12a985
jgomezz <jgomezz@gmail.com>
2023-11-26 10:33:25 -0500

    Update

---
+++ src/main/java/pe/edu/tecsup/springbootapp/beans/SaludoInglesImpl.java
@@ -0,0 +1,25 @@
+package pe.edu.tecsup.springbootapp.beans;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Primary
+@Component
+public class SaludoInglesImpl implements Saludo{
+
+	@Override
+	public String dia(String nombres) {
+		return "Good morning " + nombres;
+	}
+
+	@Override
+	public String tarde(String nombres) {
+		return "Good Afternoon " + nombres;
+	}
+
+	@Override
+	public String noche(String nombres) {
+		return "Good evening " + nombres;
+	}
+
+}
